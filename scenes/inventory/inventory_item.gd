df81f318b638cc0d77f6225d579c9db92c951867
businessgoose555 <businessgoose555@gmail.com>
2025-10-22 09:31:50 +0200

    fims

--- scenes/inventory/inventory_item.gd
+++
@@ -1,9 +0,0 @@
-class_name InventoryItem  extends RefCounted
-
-var item_data: ItemData
-var quantity: int = 0
-
-func _init(p_item_data: ItemData, p_quantity: int) -> void:
-	item_data = p_item_data
-	quantity = p_quantity
-	
--- scripts/global.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var save_dataarchaic:SaveData
-
-func _ready():
-	save_dataarchaic = SaveData.load_or_create()
-	
-var score: int = 0
